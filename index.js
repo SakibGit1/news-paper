1a201738eb4c719ec5f33783be5c1f685070ab08
Sakib Islam <bachelorsakib@gmail.com>
2023-08-31 00:37:20 +0600

    author

--- index.js
+++ index.js
@@ -32,18 +32,34 @@ const handleLoadNews = async (categoryId) => {
         console.log(news);
         const div = document.createElement('div');
         div.innerHTML = `
-            <div class="card w-96 bg-base-100 shadow-xl">
+            <div class="card bg-base-100 shadow-xl">
              <figure><img src="${news?.image_url
              }" alt="Shoes" /></figure>
             <div class="card-body">
             <h2 class="card-title">
-            Shoes!
-            <div class="badge badge-secondary">NEW</div>
+            ${news.title.slice(0,40)}
+            <div class="badge badge-secondary py-4">${news?.rating?.badge}</div>
             </h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-            <div class="badge badge-outline">Fashion</div> 
-            <div class="badge badge-outline">Products</div>
+            <p>${news.details.slice(0,80)}</p>
+            <h3 class="text-xm font-semibold"> total views: ${news.total_view?news.total_view:"no views"}</h3>
+            <div class="card-actions flex justify-between mt-4">
+
+             <div class="avatar gap-2 flex items-center">
+        <div class="w-14 rounded-full">
+            <img src="${news.author?.img}" />
+                  
+            </div>
+           <div>
+           <h6>${news.author?.name.slice(0,6)}</h6>
+           
+           </div>
+  
+      
+             </div>
+
+             <div>
+             <button class="btn bg-blue-100">Details</button>
+             </div>
             </div>
             </div>
         </div>
